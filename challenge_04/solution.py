eed2f2a4cf793e1da81e64275d5c53b12327052c
Tiago Gomes <tiago.gomes.ext@altran.com>
2023-02-22 15:43:38 +0000

    Added first try to solve the challenge, but there're some tests not passing:
    Test 1 failed
    Test 2 passed!
    Test 3 passed! [Hidden]
    Test 4 passed! [Hidden]
    Test 5 failed  [Hidden]
    Test 6 passed! [Hidden]
    Test 7 passed! [Hidden]
    Test 8 passed! [Hidden]
    Test 9 failed  [Hidden]
    Test 10 passed! [Hidden]

--- challenge_04/solution.py
+++ challenge_04/solution.py
@@ -1,4 +1,4 @@
-def decompose_in_sum_two_terms(number, start_number):
+def decompose_in_sum_two_terms(start_number, number):
     if start_number > number:
         return 0
 
@@ -7,24 +7,32 @@ def decompose_in_sum_two_terms(number, start_number):
     y = number - start_number
 
     while(x < y):
+        print(x,y)
+        n_items += decompose_in_sum_two_terms(x+1, y) + 1
         x += 1
         y -= 1
-        n_items += 1
 
     return n_items
 
 def solution(n):
-    return decompose_in_sum_two_terms(n, 1)
+    return decompose_in_sum_two_terms(1, n)
 
 
 def main():
-    print(solution(3))
-    print(solution(4))
-    print(solution(5))
-    print(solution(6))
-    print(solution(7))
-    print(solution(8))
-    print(solution(9))
+    # print(3, solution(3))
+    # print(4, solution(4))
+    # print(5, solution(5))
+    # print(6, solution(6))
+    # print(7, solution(7))
+    # print(8, solution(8))
+    # print(9, solution(9))
+    # print(9, solution(50))
+
+    for i in range(0, 15):
+        print(i, solution(i))
+        
+
+    # print(solution(200))
     # print(solution(200) == 487067745)
     # print(solution(3) == 1)
 
