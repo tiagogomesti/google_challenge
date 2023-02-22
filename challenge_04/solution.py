ba71075077f1428d9a3408f870100c4b4b809900
Tiago Gomes <tiago.gomes.ext@altran.com>
2023-02-22 13:59:51 +0000

    Added function to decompose a number in a sum of two terms

--- challenge_04/solution.py
+++ challenge_04/solution.py
@@ -1,2 +1,32 @@
+def decompose_in_sum_two_terms(number, start_number):
+    if start_number > number:
+        return 0
+
+    n_items = 0
+    x = start_number
+    y = number - start_number
+
+    while(x < y):
+        x += 1
+        y -= 1
+        n_items += 1
+
+    return n_items
+
 def solution(n):
-    # Your code here
+    return decompose_in_sum_two_terms(n, 1)
+
+
+def main():
+    print(solution(3))
+    print(solution(4))
+    print(solution(5))
+    print(solution(6))
+    print(solution(7))
+    print(solution(8))
+    print(solution(9))
+    # print(solution(200) == 487067745)
+    # print(solution(3) == 1)
+
+if __name__ == "__main__":
+    main()    
